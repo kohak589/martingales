2bb3c7550e53a45ca59574f45b8bc83e61853a21
kohak589 <kohak213@gmail.com>
2018-10-31 14:32:04 +0900

    update code

--- Probabilitywithmartingales.py
+++ Probabilitywithmartingales.py
@@ -28,22 +28,25 @@ def game():
         #print('=====[You lose]=====')
         lose()
 
-def money(what,x=0):
-    #x=1　の時は所持金を掛け金が超えないようにする処理
+def money(what,x):
+    #x=1 の時は所持金を掛け金が超えないようにする処理
+    #x=2 の時は目標額が所持金を下回らないようにする処理
     global numbers
-    global sub
     while True:
         yourInput = input(what + 'を入力してください：')
         if yourInput.isdigit():
             if x == 1 and numbers[0] >= float(yourInput):
-                numbers[0] -= float(yourInput)
+                #numbers[0] -= float(yourInput)
+                pass
             elif x == 1:
                 print('所持金を超えています')
+                continue
+            elif x == 2 and numbers[0] >= float(yourInput):
+                print('既に達成しています')
                 continue
             print(what + 'は' + yourInput + '円に決められました')
             numbers.append(float(yourInput))
             return float(yourInput)
-            break
 
 def Main():
     while True:
@@ -58,10 +61,12 @@ numbers =[]
 graphY = []
 count = 0
 
-reMoney = money('所持金')
-reBet = money('掛け金',1)
-reGoal = money('目標額')
+reMoney = money('所持金',0)
+reBet = money('賭け金',1)
+reGoal = money('目標額',2)
 Main()
+graphY.append(numbers[0])
+count += 1
 
 graphX = [i for i in range(count)]
 plt.plot(graphX, graphY)
